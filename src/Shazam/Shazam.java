e5f65f174002ee46ba07978793cdc854145b45c9
mpdg837 <78957807+mpdg837@users.noreply.github.com>
2022-01-05 10:31:49 +0100

    Stworzenie spektrogramu 2

--- src/Shazam/Shazam.java
+++ src/Shazam/Shazam.java
@@ -1,57 +1,131 @@
 package Shazam;
 
+import main.java.io.honerlaw.audio.fingerprint.AudioFile;
+import main.java.io.honerlaw.audio.fingerprint.hash.FingerPrint;
+import main.java.io.honerlaw.audio.fingerprint.hash.Spectrogram;
+import main.java.io.honerlaw.audio.fingerprint.hash.peak.HashedPeak;
+import main.java.io.honerlaw.audio.fingerprint.util.Hash;
+
 import javax.imageio.ImageIO;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Shazam {
 
-    private static final int RECORDER_BPP = 16;
-    private static final int RECORDER_SAMPLERATE = 44100;
-    private static final int FFT_SAMPLE_SIZE = 4096;
+    public static void writeAudioToWavFile(byte[] data, AudioFormat format, String fn) throws Exception {
+        AudioInputStream ais = new AudioInputStream(new ByteArrayInputStream(data), format, data.length);
+        AudioSystem.write(ais, AudioFileFormat.Type.WAVE, new File(fn));
+    }
 
-
-
-    public Shazam() {
-        byte[] audioBytes = new byte[1];
-
+    public void record(){
+        AudioFormat format = new AudioFormat(44100, 16, 1, true, true);
+        TargetDataLine microphone;
         try {
+            microphone = AudioSystem.getTargetDataLine(format);
+
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            microphone = (TargetDataLine) AudioSystem.getLine(info);
+            microphone.open(format);
+
             ByteArrayOutputStream out = new ByteArrayOutputStream();
-            BufferedInputStream in = new BufferedInputStream(new FileInputStream("sample.wav"));
+            int numBytesRead;
+            byte[] data = new byte[microphone.getBufferSize() / 5];
+            microphone.start();
 
-            int read;
-            byte[] buff = new byte[1024];
-            while ((read = in.read(buff)) > 0) {
-                out.write(buff, 0, read);
+            int bytesRead = 0;
+
+            try {
+                while (bytesRead < 1000000) { //Just so I can test if recording my mic works...
+                    numBytesRead = microphone.read(data, 0, data.length);
+                    bytesRead = bytesRead + numBytesRead;
+                    //    System.out.println(bytesRead);
+                    out.write(data, 0, numBytesRead);
+
+                }
+                microphone.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            out.flush();
-            audioBytes = out.toByteArray();
 
+            writeAudioToWavFile(out.toByteArray(),format,"recorded.wav");
 
-        } catch (Exception err) {
-
-        }
-
-        Spectogram spect = new Spectogram(audioBytes);
-
-        double[][] ob = spect.getValues();
-
-
-        File outputfile = new File("image.png");
-        try {
-            ImageIO.write(spect.drawSpectrogram(), "png", outputfile);
-        }catch (IOException err){
-
+        }catch ( LineUnavailableException err){
+            err.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
 
+    public HashedPeak[] getPoints(String nazwa){
+        record();
+        File toFile = new File(nazwa);
+
+        try {
+            Spectrogram spect = new Spectrogram(new AudioFile(toFile));
+            spect.render(toFile.getName()+"spect.png");
+            FingerPrint fingerprint = new FingerPrint(new AudioFile(toFile));
+
+            HashedPeak[] hashes = fingerprint.getHashes();
+
+            System.out.println(hashes.length);
+
+            return hashes;
+        }catch (Exception err){
+            err.printStackTrace();
+            return null;
+        }
 
 
+    }
+    public Shazam() {
+
+        record();
+        HashedPeak[] recorded = getPoints("recorded.wav");
+        HashedPeak[] sample = getPoints("sample.wav");
+
+        ArrayList<HashedPeak> detected = new ArrayList<>();
+
+        int detects = 0;
+        for(int x=0;x<recorded.length;x++){
+            HashedPeak recordedpoints = recorded[x];
+            if(recordedpoints.getPeakOne().getFreq()>5 && recordedpoints.getPeakOne().getFreq()<300 ) {
+                if(recordedpoints.getPeakTwo().getFreq()>5 && recordedpoints.getPeakTwo().getFreq()<300 ) {
+
+                for (int y = 0; y < sample.length; y++) {
+                    HashedPeak samplePoints = sample[y];
+                    if(samplePoints.getPeakOne().getFreq()>5 && samplePoints.getPeakOne().getFreq()<300 ) {
+                        if (samplePoints.getPeakTwo().getFreq() > 5 && samplePoints.getPeakTwo().getFreq() < 300) {
+                            if (!detected.contains(samplePoints)) {
+
+                                if (samplePoints.getPeakOne().getFreq() == recordedpoints.getPeakOne().getFreq()) {
+
+                                    if (samplePoints.getPeakTwo().getFreq() == recordedpoints.getPeakTwo().getFreq()) {
+
+                                        if (samplePoints.getDelta() == recordedpoints.getDelta()) {
+
+                                            detected.add(samplePoints);
+                                            detects++;
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    }
+                }
+
+            }
+        }
+        System.out.println(detects);
+
+    }
 
 
     public static void main(String[] args){
